6ec8a7dee2701689af7713a037fbabd7c8dab9b1
Shannkar022 <shankarweb22@gmail.com>
2021-10-21 11:26:40 +0530

    JUnit 4 in action.

--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -5,9 +5,14 @@ import static org.junit.Assert.*;
 public class StringHelperTest {
 
     @Test
-    public void testTruncateAInFirst2Positions() {
+    public void testTruncateAInFirst2Positions_AInFirst2Positions() {
         StringHelper helper = new StringHelper();
         assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions2_AInFirstPosition() {
+        StringHelper helper = new StringHelper();
         assertEquals("CD",helper.truncateAInFirst2Positions("ACD"));
     }
 }
